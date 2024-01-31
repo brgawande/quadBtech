0e803ce3dcc31f86837177a72d0294d4b880dc82
Bhushan Gawande <brgawande@gmail.com>
2024-01-31 23:45:08 +0530

    basic styling added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["19.5px", "16px"],
+      lg: ["18px", "21px"],
+      xl: ["20px", "24px"],
+      "2xl": ["24px", "29px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "8xl": ["96px", "106px"],
+    },
+    extend: {
+      colors: {
+        primary: "ECEEFF",
+      },
+    },
   },
   plugins: [],
 };
